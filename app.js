2cc0160720d268324351f38c8bf0b2cc01e76251
Ethan Salonga <ethansalonga28@gmail.com>
2022-12-24 19:06:55 -0500

    Set up milliseconds timer

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ let cancelId
 let startTime
 const countdown = 25 * 60 * 1000
 
+const millisTimer = document.querySelector(".timer__milliseconds")
+const secondsTimer = document.querySelector(".timer__seconds")
+const minutesTimer = document.querySelector(".timer__minutes")
+
 function startTimer() {
   startTime = Date.now()
   cancelId = requestAnimationFrame(updateTimer)
@@ -18,6 +22,10 @@ function resetTimer() {
 function updateTimer() {
   let millisElapsed = Date.now() - startTime
   let millisLeft = countdown - millisElapsed
+
+  let millisText = millisLeft % 1000
+
+  millisTimer.innerHTML = millisText
 
   cancelId = requestAnimationFrame(updateTimer)
 }
